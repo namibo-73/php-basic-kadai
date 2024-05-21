d700251b99fc9ed25d60233fd8cb1688f5b83f8c
NAMI <143819580+namibo-73@users.noreply.github.com>
2024-05-21 18:49:41 +0900

    課題提出

---
+++ kadai_007/associative-array.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <title>PHP課題</title>
+</head>
+<body>
+    <?php
+    $personal_data = ['name' => 'onion', 'price' => '200', 'weight' => '160'];
+    // 配列の値を出力する
+    print_r($personal_data);
+    ?>
+</body>
+</html>
